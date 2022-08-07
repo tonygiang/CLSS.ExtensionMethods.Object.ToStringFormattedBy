6716b904f19678418994616a3c63977d54a6a041
tonygiang <tony_s_giang@yahoo.com>
2022-08-07 13:41:43 +0700

    Project push.

---
+++ Object.ToStringFormattedBy.cs
@@ -0,0 +1,190 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class ObjectToStringFormattedBy
+  {
+    /// <summary>
+    /// Inserts the source object's string representation into the specified
+    /// format's format item index 0. Objects passed in after the format string
+    /// are additionally inserted into following format items.
+    /// A parameter supplies culture-specific formatting information.
+    /// </summary>
+    /// <param name="arg0">The first object to format.</param>
+    /// <param name="provider">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='provider']"/></param>
+    /// <param name="format">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='format']"/></param>
+    /// <param name="args">An object array that contains zero or more objects to
+    /// format after the source object.</param>
+    /// <returns>A copy of <paramref name="format"/> in which the format items
+    /// have been replaced by the string representation of the corresponding
+    /// objects starting with the source object and then by objects in
+    /// <paramref name="args"/>.</returns>
+    public static string ToStringFormattedBy(this object arg0,
+      IFormatProvider provider,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0) return String.Format(provider, format, arg0);
+      var combinedArgs = new object[args.Length + 1];
+      combinedArgs[0] = arg0;
+      for (int i = 0; i < args.Length; ++i) combinedArgs[i + 1] = args[i];
+      return String.Format(provider, format, combinedArgs);
+    }
+
+    /// <summary>
+    /// Inserts the source object's string representation into the specified
+    /// format's format item index 0. Objects passed in after the format string
+    /// are additionally inserted into following format items.
+    /// </summary>
+    /// <param name="arg0">
+    /// <inheritdoc cref="ToStringFormattedBy(object, IFormatProvider, string, object[])"
+    /// path="/param[@name='arg0']"/></param>
+    /// <param name="format">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='format']"/></param>
+    /// <param name="args">
+    /// <inheritdoc cref="ToStringFormattedBy(object, IFormatProvider, string, object[])"
+    /// path="/param[@name='args']"/></param>
+    /// <returns>
+    /// <inheritdoc cref="ToStringFormattedBy(object, IFormatProvider, string, object[])"
+    /// path="/returns"/></returns>
+    public static string ToStringFormattedBy(this object arg0,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0) return String.Format(format, arg0);
+      var combinedArgs = new object[args.Length + 1];
+      combinedArgs[0] = arg0;
+      for (int i = 0; i < args.Length; ++i) combinedArgs[i + 1] = args[i];
+      return String.Format(format, combinedArgs);
+    }
+
+    /// <summary>
+    /// Inserts the source collection's object string representations into the
+    /// specified format's format items starting at index 0, then additionally
+    /// does the same to objects passed in after the format string starting
+    /// after the source array.
+    /// A parameter supplies culture-specific formatting information.
+    /// </summary>
+    /// <param name="sourceArgs">The first objects to format.</param>
+    /// <param name="provider">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='provider']"/></param>
+    /// <param name="format">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='format']"/></param>
+    /// <param name="args">An object array that contains zero or more objects to
+    /// format after the source's objects.</param>
+    /// <returns>A copy of <paramref name="format"/> in which the format items
+    /// have been replaced by the string representation of the corresponding
+    /// objects starting with the source's objects and then by objects in
+    /// <paramref name="args"/>.</returns>
+    public static string ToStringFormattedBy(
+      this ICollection<object> sourceArgs,
+      IFormatProvider provider,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0)
+      {
+        var sourceAsArray = sourceArgs as object[];
+        if (sourceAsArray != null)
+          return String.Format(provider, format, sourceAsArray);
+      }
+      var combinedArgs = new object[args.Length + sourceArgs.Count];
+      int sourceIdx = 0;
+      foreach (var arg in sourceArgs)
+      { combinedArgs[sourceIdx] = arg; ++sourceIdx; }
+      for (int i = 0; i < args.Length; ++i)
+        combinedArgs[i + sourceArgs.Count] = args[i];
+      return String.Format(provider, format, combinedArgs);
+    }
+
+    /// <inheritdoc cref="ToStringFormattedBy(ICollection{object}, IFormatProvider, string, object[])"/>
+    public static string ToStringFormattedBy(this ICollection sourceArgs,
+      IFormatProvider provider,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0)
+      {
+        var sourceAsArray = sourceArgs as object[];
+        if (sourceAsArray != null)
+          return String.Format(provider, format, sourceAsArray);
+      }
+      var combinedArgs = new object[args.Length + sourceArgs.Count];
+      int sourceIdx = 0;
+      foreach (var arg in sourceArgs)
+      { combinedArgs[sourceIdx] = arg; ++sourceIdx; }
+      for (int i = 0; i < args.Length; ++i)
+        combinedArgs[i + sourceArgs.Count] = args[i];
+      return String.Format(provider, format, combinedArgs);
+    }
+
+    /// <summary>
+    /// Inserts the source collection's object string representations into the
+    /// specified format's format items starting at index 0, then additionally
+    /// does the same to objects passed in after the format string starting
+    /// after the source array.
+    /// </summary>
+    /// <param name="sourceArgs">
+    /// <inheritdoc cref="ToStringFormattedBy(ICollection{object}, IFormatProvider, string, object[])"
+    /// path="/param[@name='sourceArgs']"/></param>
+    /// <param name="format">
+    /// <inheritdoc cref="String.Format(IFormatProvider, string, object[])"
+    /// path="/param[@name='format']"/></param>
+    /// <param name="args">
+    /// <inheritdoc cref="ToStringFormattedBy(ICollection{object}, IFormatProvider, string, object[])"
+    /// path="/param[@name='args']"/></param>
+    /// <returns>
+    /// <inheritdoc cref="ToStringFormattedBy(ICollection{object}, IFormatProvider, string, object[])"
+    /// path="/returns"/></returns>
+    public static string ToStringFormattedBy(
+      this ICollection<object> sourceArgs,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0)
+      {
+        var sourceAsArray = sourceArgs as object[];
+        if (sourceAsArray != null)
+          return String.Format(format, sourceAsArray);
+      }
+      var combinedArgs = new object[args.Length + sourceArgs.Count];
+      int sourceIdx = 0;
+      foreach (var arg in sourceArgs)
+      { combinedArgs[sourceIdx] = arg; ++sourceIdx; }
+      for (int i = 0; i < args.Length; ++i)
+        combinedArgs[i + sourceArgs.Count] = args[i];
+      return String.Format(format, combinedArgs);
+    }
+
+    /// <inheritdoc cref="ToStringFormattedBy(ICollection, IFormatProvider, string, object[])"/>
+    public static string ToStringFormattedBy(this ICollection sourceArgs,
+      string format,
+      params object[] args)
+    {
+      if (args.Length == 0)
+      {
+        var sourceAsArray = sourceArgs as object[];
+        if (sourceAsArray != null)
+          return String.Format(format, sourceAsArray);
+      }
+      var combinedArgs = new object[args.Length + sourceArgs.Count];
+      int sourceIdx = 0;
+      foreach (var arg in sourceArgs)
+      { combinedArgs[sourceIdx] = arg; ++sourceIdx; }
+      for (int i = 0; i < args.Length; ++i)
+        combinedArgs[i + sourceArgs.Count] = args[i];
+      return String.Format(format, combinedArgs);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+﻿# CLSS.ExtensionMethods.Object.ToStringFormattedBy
+
+### Problem
+
+[`System.String.Format`](https://docs.microsoft.com/en-us/dotnet/api/system.string.format?view=net-6.0) fulfills its role well in a general context. However, its syntax does not particularly lend itself well to functional-style call chain.
+
+```
+using Newtonsoft.Json.Linq;
+
+var queriedNode = JToken.Parse(rawJSON)
+  .SelectToken(jsonPath)
+  .Value<bool>();
+var deprecationLabel = String.Format("This is an deprecated entry? {0}", queriedNode);
+```
+
+### Solution
+
+`ToStringFormattedBy` is an extension method created with functional syntax in mind. In your code, it conveys the intent of transforming an object into string form in a particular format more expressively.
+
+```
+using CLSS;
+using Newtonsoft.Json.Linq;
+
+var deprecationLabel = JToken.Parse(rawJSON)
+  .SelectToken(jsonPath)
+  .Value<bool>()
+  .ToStringFormattedBy("This is an deprecated entry? {0}");
+```
+
+The format string taken in by `ToStringFormattedBy` follows the same rules and syntax of [composite formatting](https://docs.microsoft.com/en-us/dotnet/standard/base-types/composite-formatting). Follow that link to learn more.
+
+`ToStringFormattedBy` also takes in trailing variadic arguments. Since the source object calling `ToStringFormattedBy` will replace format item at index 0, trailing arguments following the format string will start at format index 1.
+
+```
+using CLSS;
+
+// convert into some kind of subtitles markup
+dialogue.ToStringFormattedBy("<color={1}>{2}</color>: {0}", character.ColorCode, character.Name);
+```
+
+Optionally, `ToStringFormattedBy` also takes in an [`IFormatProvider`](https://docs.microsoft.com/en-us/dotnet/api/system.iformatprovider?view=net-6.0) if passed in before the format string. This overload also takes in trailing variadic arguments.
+
+```
+using CLSS;
+
+today.ToStringFormattedBy(customCultureInfo, "Today is {0}");
+```
+
+You can also call `ToStringFormattedBy` on any [`ICollection`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.icollection?view=net-6.0) or [`ICollection<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.icollection-1?view=net-6.0). The format items in the format string will map to elements in the source array in order of index number. Any trailing variadic argument the overloads for array take in will map to format items starting at the index number equivalent to the source collection's length.
+
+```
+using CLSS;
+
+var versionDigits = new int[] { 1, 5, 3 };
+var versionString = versionDigits.ToStringFormattedBy("{0}.{1}.{2}"); // "1.5.3"
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
